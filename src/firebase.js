7cec21e4de189f8c3651a89bffb20c5814a78545
Mrswami <65364622+Mrswami@users.noreply.github.com>
2026-02-23 18:01:45 -0600

    fix: wire real firebase credentials + fix CI notify job

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace with your actual Firebase config from the console
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDDHqjkRJY_fHI-0hqUiJu8GYIxjjQI2bk",
+    authDomain: "spotifyunlocked.firebaseapp.com",
+    projectId: "spotifyunlocked",
+    storageBucket: "spotifyunlocked.firebasestorage.app",
+    messagingSenderId: "504617755413",
+    appId: "1:504617755413:web:de9c59204d2b91cdfeff1c",
+    measurementId: "G-M59C1FKFXR"
 };
 
 const app = initializeApp(firebaseConfig);
